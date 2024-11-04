f57bfc991c38d63e4d8022f7f37db14fdb179e8d
Rishu Jaiswal <132575289+RishuJaiz@users.noreply.github.com>
2024-11-04 21:48:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".text1", {
+    strings: ["Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+
+
+
+//   var typed = new Typed(".text1", {
+//     strings: ["Developer", "Blogger", "Designer", "Freelancer"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// });
